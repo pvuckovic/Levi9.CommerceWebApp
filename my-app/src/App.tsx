2c76fd3c29b1e8f34edbbc55d4ef85814aeb5dbf
Stevan <61841410+AleteiaSD@users.noreply.github.com>
2023-05-19 15:54:26 +0200

    Added products page (#3)
    Added products page and create routes.

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,13 +1,18 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
+import NavigationBar from "./components/navigationBar";
 import DocumentPage from "./components/document";
+import Product from "./components/product/products"
 
 function App() {
   return (
-    <div className="App">
-      <Router>
+    <div className="App">      
+      <Router> 
+      <NavigationBar /> 
         <Routes>
-          <Route path="/document" element={<DocumentPage />} />
+          <Route path="/" element={<Product/>} />
+          <Route path="/products" element={<Product/>} />
+          <Route path="/document" element={<DocumentPage/>} /> 
         </Routes>
       </Router>
     </div>
